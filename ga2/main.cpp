b2717672ce32701576bffca87235f27eb0f5305a
Victor Rangel <vicjavirangel@gmail.com>
2019-09-12 12:42:02 -0500

    Initial Commit

---
+++ ga2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ga2
+//
+//  Created by Victor Rangel on 9/12/19.
+//  Copyright © 2019 Victor Rangel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
