31137bc0535d2fac41aed555b30697186d839408
Daniel Aguila <47072469+Daniel-Aguila@users.noreply.github.com>
2019-09-12 12:52:01 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/basketball.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for basketball.
+# This may be replaced when dependencies are built.
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Player{
+private:
+    int number;
+    int age;
+    int minutes;
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
