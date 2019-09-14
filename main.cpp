b75c495c8c3dc1b37dcc94fb495c9d6f54f35b8f
Daniel Aguila <daguila658@gmail.com>
2019-09-14 00:57:16 -0500

    skeleton

--- main.cpp
+++ main.cpp
@@ -1,13 +1,42 @@
 #include <iostream>
+using namespace std;
 
+//Parent Class
 class Player{
 private:
-    int number;
+    int number = 1;
     int age;
     int minutes;
+public:
+
+};
+//Child Class
+class courtPlayer: public Player{
+private:
+public:
+};
+//Child Class
+class BenchPlayer: public Player{
+
 };
 
+//Linked List
+struct Node{
+    int data;
+    Node* next;
+};
+class list{
+private:
+    Node*head;
+    Node*tail;
+public:
+    list();
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
+}
+
+list::list() {
+    head = NULL;
+    tail = NULL;
 }
