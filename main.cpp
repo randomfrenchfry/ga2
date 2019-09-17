60b8454729aeb016d4e931c58fd148df2abecf6e
Daniel Aguila <daguila658@gmail.com>
2019-09-16 23:54:42 -0500

    FIX ME: create a Player, and create the relationship between Node, and classes

--- main.cpp
+++ main.cpp
@@ -4,12 +4,23 @@ using namespace std;
 //Parent Class
 class Player{
 private:
-    int number = 1;
+    int number;
     int age;
     int minutes;
 public:
-
+    Player(int num,int age, int min);
+    void setNumber(int num){this->number = num;}
+    void setAge(int age){this->age = age;}
+    void setMinutes(int min){this->minutes = min;}
+    int getNumber(){return number;}
+    int getAge(){return age;}
 };
+
+Player::Player(int num, int age, int min) {
+    this->number = num;
+    this->age = age;
+    this->minutes = min;
+}
 //Child Class
 class courtPlayer: public Player{
 private:
@@ -22,7 +33,7 @@ class BenchPlayer: public Player{
 
 //Linked List
 struct Node{
-    int data;
+    Player data;
     Node* next;
 };
 class list{
@@ -31,12 +42,27 @@ private:
     Node*tail;
 public:
     list();
+    void createPlayer(int,int,int);
 };
+
 int main() {
+
     return 0;
 }
 
 list::list() {
     head = NULL;
     tail = NULL;
+}
+
+void list::createPlayer(int num,int age, int min){
+    Player
+    Node*p = new Node;
+    p->data.setNumber(num);
+
+    if(head == NULL){
+        head = p;
+        tail = p;
+        p = NULL;
+    }
 }
