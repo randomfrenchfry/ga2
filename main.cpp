2cafffe195096015c851f9bc2e5a798b6ad52462
vjrange3 <49768022+vjrange3@users.noreply.github.com>
2019-09-19 15:54:22 -0500

    did some stuff...Xcode is horrible with GitHub

--- main.cpp
+++ main.cpp
@@ -9,31 +9,31 @@ public:
     int number;
     int age;
     int minutes;
-
+    
     bool isOnCourt;
-
+    
     Player* next;
     Player* prev;
-
+    
     Player() {
         this->number = -1;
         this->age = -1;
         this->minutes = -1;
-
+        
         this->isOnCourt = false;
-
+        
         this->next = NULL;
         this->prev = NULL;
     }
-
+    
     Player(int number, int age, int minutes, bool isOnCourt)
     {
         this->number = number;
         this->age = age;
         this->minutes = minutes;
-
+        
         this->isOnCourt = isOnCourt;
-
+        
         this->next = NULL;
         this->prev = NULL;
     }
@@ -44,17 +44,31 @@ class Bench
 public:
     Player* head;
     Player* tail;
-
+    int size;
+    
     Bench()
     {
         this->head = NULL;
         this->tail = NULL;
+        this->size =0;
     }
-
-    void addFirst() {
-
+    
+    void addFirst(Player *x) //done I think
+    {
+        if (head == NULL)
+        {
+            head = x;
+            tail = x;
+            
+            return;
+        }
+        x->next = head;
+        head = x;
+        ++size;
+        
+        
     }
-
+    
     // call addLast
     void add(Player* x)
     {
@@ -62,45 +76,67 @@ public:
         {
             head = x;
             tail = x;
-
+            
             return;
         }
-
+        
         x->prev = tail;
         tail->next = x;
         tail = x;
+        ++size;
     }
-
-    void getFirst() {
-
+    
+    Player* getFirst()//why?
+    {
+        
+        return head;
+        
     }
-
-    void getLast() {
-
+    
+    Player* getLast()//why?
+    {
+        return tail;
     }
-
-
-    void get() {
-
+    
+    
+    Player* get(int index)
+    {
+        if (head == NULL)
+            return NULL;
+        if (index < 0 || index >= size)
+            return NULL;
+        
+        Player* tmp = head;
+        int counter = 0;
+        while (tmp != NULL) {
+            if (counter == index)
+                return tmp;
+            
+            tmp = tmp->next;
+            ++counter;
+        }
+        
+        return NULL;
     }
-
-    void set() {
-
+    
+    void set(Player* x)// change bench status?
+    {
+        x->isOnCourt = !x->isOnCourt;
     }
-
+    
     void print()
     {
         cout << "This is the bench: ";
-
+        
         Player* tmp = head;
-
+        
         while (tmp != NULL) {
             cout << tmp->number;
             if (tmp->next != NULL)
                 cout << " --> ";
             tmp = tmp->next;
         }
-
+        
         cout << endl;
     }
 };
@@ -111,62 +147,71 @@ public:
     Player* head;
     Player* tail;
     int size;
-
+    
     Court()
     {
         this->head = NULL;
         this->tail = NULL;
         this->size = 0;
     }
-
+    
     void add(Player* x)
     {
         ++size;
-
+        
         if (head == NULL)
         {
             head = x;
             tail = x;
-
+            
             return;
         }
-
+        
         x->prev = tail;
         tail->next = x;
         tail = x;
         tail->next = head;
     }
-
-    void set() {
-
+    
+    void set(int index, int minutesPlayed)//maybe idk
+    {
+        Player* tmp = head;
+        int counter = 0;
+        while (tmp != NULL) {
+            if (counter == index)
+                tmp->minutes = minutesPlayed;
+            
+            tmp = tmp->next;
+            ++counter;
+        }
     }
-
+    
     Player* get(int index)
     {
         if (head == NULL)
             return NULL;
         if (index < 0 || index >= size)
             return NULL;
-
+        
         Player* tmp = head;
         int counter = 0;
         while (tmp != NULL) {
             if (counter == index)
                 return tmp;
-
+            
             tmp = tmp->next;
             ++counter;
         }
-
+        
         return NULL;
     }
-
+    
     void print()
     {
         cout << "This is the court: ";
-
+        
         Player* tmp = head;
-
+        
         while (tmp != NULL) {
             cout << tmp->number;
             if (tmp->next != head)
@@ -175,17 +220,17 @@ public:
                 break;
             tmp = tmp->next;
         }
-
+        
         cout << endl;
     }
-
+    
     void sort(string sortBy)
     {
         for (int i = 0; i < size - 1; i++)
             for (int j = 0; j < size - 1; j++) {
                 Player* x = get(j);
                 Player* y = get(j + 1);
-
+                
                 //  4-> 7-> 1
                 // &1->&2->&3
                 if (sortBy == "number" && x->number > y->number) {
@@ -210,23 +255,28 @@ public:
 int main()
 {
     Player* lockerRoom = new Player[12];
-
+    
     Bench bench;
     for (int i = 6; i <= 12; i++) {
         Player* p = new Player(i, i + 19 + i, 0, false);
         bench.add(p);
     }
     bench.print();
-
+    
     Court court;
     for (int i = 1; i <= 5; i++) {
-        Player* p = new Player((i + 1), i, 0, false);
+        Player* p = new Player((i + 1), i, 0, true);
         court.add(p);
     }
-
+    
     court.sort("age");
     court.print();
-
-
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
